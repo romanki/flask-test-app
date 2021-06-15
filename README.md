181480c0dd43de1d0c372574add5e7c85a4c08c7
Roman <roman@anodot.com>
2021-06-15 11:54:07 +0300

    add basic flask webapp

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# flask-test-app
+# Flask Test Web App
+
+## Setup & Installtion
+
+```bash
+git clone <repo-url>
+```
+
+DB should be installed for the app: 
+
+```bash
+docker run --name postgres -e POSTGRES_PASSWORD={DEFINE_PASSWORD} -p 5432:5432 -d postgres
+```
+Install requirements
+```bash
+pip3 install -r requirements.txt
+```
+
+## Running The App
+
+```bash
+python main.py
+```
+
+## Viewing The App
+
+Go to `http://127.0.0.1:5000`
---
+++ website/__init__.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'rfvtgbyhn edcrfvtgb'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:p0ssw0rd@localhost/postgres'
+    # Pooling control
+    app.config['SQLALCHEMY_POOL_SIZE'] = 10
+    # logging of DB connection
+    app.config['SQLALCHEMY_ECHO'] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User, Note
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
+
+
+def create_database(app):
+    db.create_all(app=app)
+    print('Created Database!')
